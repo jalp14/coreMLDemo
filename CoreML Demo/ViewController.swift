cb408fc3f4b7f84a04eac534952108ff6c31e5c1
Jalp Desai <jalpdesai@icloud.com>
2018-11-28 21:59:58 +0000

    Initial Commit

---
+++ CoreML Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML Demo
+//
+//  Created by Jalp on 28/11/2018.
+//  Copyright © 2018 JDC0rp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
